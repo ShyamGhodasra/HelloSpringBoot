c0b4329f36adaf0262598549e25a6fdb126eafda
Shyam Ghodasra <shyamghodasra@gmail.com>
2021-05-13 11:51:36 +0000

    Added Main File

---
+++ src/main/java/com/test/HellobootApplication.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobootApplication.class, args);
+	}
+
+	@RestController
+	static final class mycontroller {
+		@GetMapping("/")
+		String hello() {
+			return "Hello";
+		}
+
+		@PostMapping("/add")
+		public String add(@RequestBody String s) {
+			System.out.println(s);
+			return "done";
+		}
+	}
+}
